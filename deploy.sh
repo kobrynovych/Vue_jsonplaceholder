c18ed2059674be390c00bc76bc1966655599bc7a
kobrynovych <kobrynovych@gmail.com>
2025-03-19 14:38:34 +0200

    test deploy

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:kobrynovych/Vue_jsonplaceholder.git master:gh-pages
+git push -f git@github.com:kobrynovych/Vue_jsonplaceholder.git main:gh-pages
 
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/Vue_jsonplaceholder/" : "/",
+    publicPath: '/Vue_jsonplaceholder/'
 }
